0a337d41f9b2f68bd7d841703af3f0a31b986bbe
本木栗 Bemly_ <2328712329@qq.com>
2022-07-02 13:24:19 +0800

    脱敏

---
+++ src/resources/textures.js
@@ -0,0 +1,128 @@
+let billboardTextures = {};
+// 第一块展板图片地址
+billboardTextures.blogTexture = '../src/jsm/blog.png';
+// 第二块展板图片地址
+billboardTextures.musicTexture =
+  '../src/jsm/music.png';
+// 第三块展板图片地址
+billboardTextures.fundTexture =
+  '../src/jsm/fund.png';
+
+let boxTexture = {};
+// 链接的图标地址
+// boxTexture.mail = '../src/jsm/envelope.png';
+// boxTexture.reactIcon = '../src/jsm/react.png';
+// boxTexture.allSkills = '../src/jsm/allSkills.png';
+// boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+// boxTexture.skrillex = '../src/jsm/skrillex.png';
+// boxTexture.edmText = '../src/jsm/EDM.png';
+
+
+// 砖块材质
+let stoneTexture = '../src/jsm/stone.webp';
+// 展板木头材质
+let woodTexture = '../src/jsm/woodTexture.webp';
+
+// 文字图片地址
+let inputText = {};
+// inputText.terpSolutionsText = '../src/jsm/terp-solutions-text.png';
+// inputText.activities = '../src/jsm/activities_text.png';
+// inputText.bagholderBetsText = '../src/jsm/bagholderbets-text.png';
+// inputText.homeSweetHomeText = '../src/jsm/home-sweet-home-text.png';
+// inputText.staticPortfolio = '../src/jsm/static-portfolio.png';
+// inputText.pcControl = '../src/jsm/pc-control.png'
+// inputText.mobileControl = '../src/jsm/mobile-control.png'
+// inputText.link = '../src/jsm/link.png'
+inputText.blogsign = '../src/jsm/blogsign.webp';
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+// 链接的跳转地址
+let URL = {};
+
+//自己
+URL.blog = 'https://citronbemly.github.io/';
+
+//国内
+URL.BiliBili = 'https://space.bilibili.com/259928976';
+boxTexture.BiliBili = '../src/jsm/pilipala.jpg';
+URL.QQ = 'https://qun.qq.com/qqweb/qunpro/share?_wv=3&_wwv=128&appChannel=share&inviteCode=xsua&appChannel=share&businessType=9&from=246610&biz=ka';
+boxTexture.QQ = '../src/jsm/QQ.jpg';
+URL.qq = 'https://qm.qq.com/cgi-bin/qm/qr?k=q6ucHSu8h3SDQEAKIWlat9ebbvl_hk9E&noverify=0';
+boxTexture.qq = '';
+URL.wechat = 'https://u.wechat.com/MCBiavAkTUX-4z9yeqEkTrQ';
+boxTexture.wechat = '';
+URL.weibo = 'https://weibo.com/u/7558867861';
+boxTexture.weibo = '';
+URL.taptap = 'https://www.taptap.com/user/3557555';
+boxTexture.taptap = '';
+URL.taptapglobal = 'https://www.taptap.io/user/464746820';
+boxTexture.taptapglobal = '';
+URL.afdian = 'https://afdian.net/u/f8d9ae60f94511ec9c1b52540025c377';
+boxTexture.afdian = '';
+URL.lofter = 'https://bemly.lofter.com/ https://lanmeixiaoguodong.lofter.com/';
+boxTexture.lofter = '';
+URL.coolapk = 'https://www.coolapk.com/u/2648780';
+boxTexture.coolapk = '';
+URL.bbs360 = 'https://bbs.360.cn/space-uid-51195310.html';
+boxTexture.bbs360 = '';
+URL.unity = 'https://developer.unity.cn/u/ben-mu-li-bemly?tab=personalDynamic';
+boxTexture.unity = '';
+
+//mc
+URL.neteaseMC = 'http://mc.netease.com/?5398';
+boxTexture.neteaseMC = '';
+URL.mcbbs = 'https://www.mcbbs.net/?1098910';
+boxTexture.mcbbs = '';
+URL.minebbs = 'https://www.minebbs.com/members/bemly_.44704/';
+boxTexture.minebbs = '';
+URL.klpbbs = 'https://klpbbs.com/space-uid-472918.html';
+boxTexture.klpbbs = '';
+
+//游戏平台
+URL.Xbox = '好友代码:blueberryLanmei';
+boxTexture.Xbox = '';
+URL.steam = 'https://steamcommunity.com/id/bemlychina';
+boxTexture.steam = '';
+URL.epic = '好友代码:本木栗Bemly_';
+boxTexture.epic = '';
+URL.apex = '好友代码:Bemly#4200';
+boxTexture.epic = '';
+URL.parsec = '好友代码:Bemly#10185871、好友代码:Bemly#10186114';
+boxTexture.parsec = '';
+URL.hoyolab = 'https://www.hoyolab.com/accountCenter/postList?id=239388997';
+boxTexture.hoyolab = '';
+
+//国外
+URL.gitHub = 'https://github.com/CitronBemly';
+boxTexture.Github = '../src/jsm/githubLogo.png';
+URL.discord = 'https://discord.gg/dy29csgeAs';
+boxTexture.discord = '';
+URL.reddit = 'https://www.reddit.com/user/bemly_';
+boxTexture.reddit = '';
+URL.twitter = 'https://twitter.com/bemly_';
+boxTexture.twitter = '';
+URL.youtuber = 'https://www.youtube.com/channel/UCPGcRCcfBIZBmT5-WIqV3Rg';
+boxTexture.youtuber = '';
+URL.telegram = 'https://t.me/citronbemly';
+boxTexture.telegram = '';
+URL.VK = 'https://vk.com/bemly';
+boxTexture.VK = '';
+URL.facebook = 'https://www.facebook.com/citronbemly';
+boxTexture.facebook = '';
+URL.mega = 'https://mega.nz/C!TtAWFDrb';
+boxTexture.mega = '';
+
+// URL.email = 'https://airhua602@gmail.com';
+// URL.music = 'https://music.huabyte.com';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
